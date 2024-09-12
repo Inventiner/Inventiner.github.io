6e26c03e9e2781e1ff1279d1b21749138b2b30bb
Inventiner <46766058+Inventiner@users.noreply.github.com>
2024-09-12 21:33:43 +0700

    updated some stuff

--- Tugas2-GameJS/scoreboard.js
+++ Tugas2-GameJS/scoreboard.js
@@ -6,7 +6,7 @@ export class scoreboard{
             .then(result => {
                 this.result = JSON.parse(result)
                 this.result.data.sort((a, b) => parseInt(b.score) - parseInt(a.score));
-                // console.log(this.result.data)
+                console.log(this.result.data)
                 document.getElementById("nama1th").innerHTML    = this.result.data[0].nama
                 document.getElementById("score1th").innerHTML   = this.result.data[0].score
                 document.getElementById("id1th").innerHTML      = this.result.data[0].id
